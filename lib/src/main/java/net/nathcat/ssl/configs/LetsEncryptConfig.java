fb4148a7556b22d6c9f7dc630c5d3971c1dca4c6
Nathan Baines <nathan.baines@nathcat.net>
2026-02-12 21:56:19 +0000

    Project init and workflow setup

--- lib/src/main/java/net/nathcat/ssl/configs/LetsEncryptConfig.java
+++ lib/src/main/java/net/nathcat/ssl/configs/LetsEncryptConfig.java
@@ -1,6 +1,12 @@
 package net.nathcat.ssl.configs;
 
 public final class LetsEncryptConfig extends Config {
+  /**
+   * The path to the private key PEM file
+   */
   public String privKeyPath;
+  /**
+   * The path to the certificate chain path
+   */
   public String certChainPath;
 }
